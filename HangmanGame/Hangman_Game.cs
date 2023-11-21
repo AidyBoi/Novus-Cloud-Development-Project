9c69a604b64780a6a776ae087678b0efbc3f14cd
AidyBoi <75675980+AidyBoi@users.noreply.github.com>
2023-11-21 13:54:53 +0000

    Add files via upload

---
+++ HangmanGame/Hangman_Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HangmanGame
+{
+    public partial class Hangman_Game : Form
+    {
+        public Hangman_Game()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddWordsBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
