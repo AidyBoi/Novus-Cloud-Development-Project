202daf7735c677b3d906e842fd8c458dec998a13
MannyAsh <105236188+MannyAsh@users.noreply.github.com>
2023-11-22 14:34:34 +0000

    connected Application

---
+++ Novus-Cloud-Development-Project-main/HangmanGame/Hangman_Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HangmanGame
+{
+    public partial class Hangman_Game : Form
+    {
+        public Hangman_Game()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GamePage gamePage = new GamePage();
+            this.Hide();
+            gamePage.ShowDialog();
+            this.Show();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddWordsBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
